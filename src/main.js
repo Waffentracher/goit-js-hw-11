6e09f70588623da089211f4e82cbbb36163085ae
Waffentracher <142826622+Waffentracher@users.noreply.github.com>
2024-05-26 07:37:18 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,46 +1,45 @@
+// main.js
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { searchImages } from './js/pixabay-api.js';
 import { renderImages, showErrorMessage } from './js/render-functions.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('search-form');
-  const searchInput = document.getElementById('search-input');
-  const gallery = document.getElementById('gallery');
-  const loader = document.getElementById('loader');
+const form = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const gallery = document.getElementById('gallery');
+const loader = document.getElementById('loader');
 
-  const lightbox = new SimpleLightbox('.gallery a');
+const lightbox = new SimpleLightbox('.gallery a');
 
-  form.addEventListener('submit', async e => {
-    e.preventDefault();
-    const searchTerm = searchInput.value.trim();
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+  const searchTerm = searchInput.value.trim();
 
-    if (!searchTerm) {
-      iziToast.error({
-        title: 'Помилка',
-        message: 'Будь ласка, введіть пошуковий запит.',
-      });
-      return;
+  if (!searchTerm) {
+    iziToast.error({
+      title: 'Помилка',
+      message: 'Будь ласка, введіть пошуковий запит.',
+    });
+    return;
+  }
+
+  loader.style.display = 'block';
+
+  try {
+    const images = await searchImages(searchTerm);
+    if (images.length === 0) {
+      showErrorMessage();
+    } else {
+      renderImages(images);
     }
-
-    loader.style.display = 'block';
-
-    try {
-      const images = await searchImages(searchTerm);
-      if (images.length === 0) {
-        showErrorMessage();
-      } else {
-        renderImages(images);
-      }
-    } catch (error) {
-      console.error('Помилка отримання зображень:', error);
-      iziToast.error({
-        title: 'Помилка',
-        message:
-          'Не вдалося отримати зображення. Будь ласка, спробуйте пізніше.',
-      });
-    } finally {
-      loader.style.display = 'none';
-    }
-  });
+  } catch (error) {
+    console.error('Помилка отримання зображень:', error);
+    iziToast.error({
+      title: 'Помилка',
+      message: 'Не вдалося отримати зображення. Будь ласка, спробуйте пізніше.',
+    });
+  } finally {
+    loader.style.display = 'none';
+  }
 });
